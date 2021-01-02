5f9bfc150e83057ad681190fbe419cb3630f7e69
Sami <sjtikka@gmail.com>
2021-01-02 11:21:27 +0000

    histogram of fully electric cars

---
+++ ajoneuvo-sähkö.R
@@ -0,0 +1,6 @@
+# find full electric cars
+tayssahko <- an[an$kayttovoima == "04",]
+hist(tayssahko$ensirekisterointipvm[tayssahko$ensirekisterointipvm > "2010-01-01"],
+     seq(as.Date("2010-01-01"), as.Date("2021-01-01"), by="year"),
+     freq = TRUE,
+     main = "Täyssähköautojen ensirekisteröinnit")
